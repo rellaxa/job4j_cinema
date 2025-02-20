05593f769c2b3f720e7d517cbc0397e898c47cd2
relaxa <ewg.nadeev@yandex.ru>
2025-02-20 18:02:14 +0300

    Filters: AuthorizationFilter, SessionFilter

---
+++ src/main/java/ru/job4j/cinema/filter/AuthorizationFilter.java
@@ -0,0 +1,34 @@
+package ru.job4j.cinema.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class AuthorizationFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        var uri = request.getRequestURI();
+        if (getTicketBuy(uri)) {
+            var userLoggedIn = request.getSession().getAttribute("user") != null;
+            if (!userLoggedIn) {
+                var loginPageUrl = request.getContextPath() + "/users/login";
+                response.sendRedirect(loginPageUrl);
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean getTicketBuy(String uri) {
+        return uri.startsWith("/tickets/buy");
+    }
+}
